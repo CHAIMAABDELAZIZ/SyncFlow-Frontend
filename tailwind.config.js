a3123bec00d66362f1d001d752986710cd7c6a36
Kahina Saadaoui <kk_saadaoui@esi.dz>
2025-04-25 15:15:04 +0100

    authentification page not responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgblue: '#789599',
+        orangePtrm: '#FF8500', 
+        redPtrm: '#DE3D28', 
+        greenPtrm: '#449A65',  
+        whitePtrm: '#EFF5F5',  
+      },
+    },
   },
   plugins: [],
 }
-
